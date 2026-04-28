3dfe3dd46605179b5c32e3282489d2afe8b4c542
sido <sidosido758@gmail.com>
2026-04-28 15:57:48 +0100

    ii

--- config/mail.js
+++ config/mail.js
@@ -1,11 +1,10 @@
-// ================= CONFIG =================
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: "toopvedeo00@gmail.com",
-    pass: "bsxb ofbu nodu qdkc"
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
   }
 });
 
