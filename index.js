1816c86194bdee73776533031376d951a03a7198
sido <sidosido758@gmail.com>
2026-04-28 02:46:10 +0100

    o

--- index.js
+++ index.js
@@ -380,8 +380,8 @@ app.get("/", (req, res) => {
 });
 
 // ================= START =================
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, "0.0.0.0", () => {
-  console.log("Server running on port", PORT);
+  console.log(`Server running on port ${PORT}`);
 });
